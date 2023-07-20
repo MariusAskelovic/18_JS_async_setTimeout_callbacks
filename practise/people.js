bf01a36224a79c69a4160ec346a490095f06bf5c
MariusAskelovic <marashz@gmail.com>
2023-07-20 13:31:40 +0300

    marriedInStrings

--- practise/people.js
+++ practise/people.js
@@ -13,8 +13,22 @@ function joinNameSurnameWithHyphen(arrName) {
     console.log(`${onePerson.name}-${onePerson.surname}`)
   );
 }
-joinNameSurnameWithHyphen(people);
+// joinNameSurnameWithHyphen(people);
 
 // 3. Parašykite funkciją, kuri atspausdina visų žmonių vardus ir pavardes
 // bei santuokos statusą, tokiu būdu Jonas Bikas: Vedęs Jonas Bikas: Nevedęs
 // Samantas Kultaitytė: Ištekėjusi Samantas Kultaitytė: Neištekėjusi
+function marriedStatusInString(arrName) {
+  arrName.map((onePerson) => {
+    if (onePerson.sex === 'male' && onePerson.married === true) {
+      console.log(`${onePerson.name} ${onePerson.surname} yra vedęs`);
+    } else if (onePerson.sex === 'male' && onePerson.married === false) {
+      console.log(`${onePerson.name} ${onePerson.surname} nėra vedęs`);
+    } else if (onePerson.sex === 'female' && onePerson.married === true) {
+      console.log(`${onePerson.name} ${onePerson.surname} yra ištekėjusi`);
+    } else if (onePerson.sex === 'female' && onePerson.married === false) {
+      console.log(`${onePerson.name} ${onePerson.surname} nėra ištekėjusi`);
+    }
+  });
+}
+marriedStatusInString(people);
