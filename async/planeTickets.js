69bd686d8fa6e1b33ec776bf990ea7102b10cd75
MariusAskelovic <marashz@gmail.com>
2023-07-20 12:00:06 +0300

    planeTickets

--- async/planeTickets.js
+++ async/planeTickets.js
@@ -1,2 +1,40 @@
 'use strict';
 console.log('planeTickets.js file was loaded');
+
+// klientas perka lektuvo bilieta
+// galimi galutiniai rezultatai
+// pavyko
+// nepavyko
+// eiga - pending
+const user1 = {
+  username: 'Mike',
+  town: 'Kaunas',
+};
+function buyPlaneTickets() {
+  return new Promise((resolve, reject) => {
+    // imituojam kad pirkimas trunka 4sek
+    setTimeout(() => {
+      // veiksmas cia vyks po 4 sekundziu
+      let isError = false;
+      console.log('isError ===', isError);
+      // jei nera klaidos,  tai pavyko
+      if (isError === false) {
+        resolve(user1);
+      } else {
+        reject();
+      }
+    }, 3000);
+  });
+}
+
+buyPlaneTickets()
+  .then((u1) => {
+    console.log('geros keliones', u1.username);
+    return u1.username.toUpperCase();
+  })
+  .then((uNameUpper) => {
+    console.log('uNameUpper ===', uNameUpper);
+  })
+  .catch(() => {
+    console.warn('ivyko klaida');
+  });
