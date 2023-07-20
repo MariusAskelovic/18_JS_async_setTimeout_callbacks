f0b1a2715fb59948c40b92f8b1f295bc0dd765e3
MariusAskelovic <marashz@gmail.com>
2023-07-20 09:35:42 +0300

    setTimeout

--- js/setTimeout.js
+++ js/setTimeout.js
@@ -1,2 +1,19 @@
 'use strict';
 console.log('setTimeout.js file was loaded');
+
+const btnEl = document.getElementById('btn');
+
+console.log('im first');
+let count = 0;
+// setTimeout((ka peleisti) , laikas milisek)
+// paleisti procesa po kazkiek laiko
+setTimeout(() => {
+  console.log('im inside setTimeout');
+}, 3000);
+
+btnEl.addEventListener('click', () => {
+  console.log('paspaudei', count++);
+});
+
+// setInterval((ka peleisti) , laikas milisek)
+// paleisti procesa kas kazkiek laiko
