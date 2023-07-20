8c5e0258bcadd1f5485857da8ae7e2a2c7ffc631
MariusAskelovic <marashz@gmail.com>
2023-07-20 09:52:56 +0300

    setInterval + clock

--- js/setTimeout.js
+++ js/setTimeout.js
@@ -2,6 +2,7 @@
 console.log('setTimeout.js file was loaded');
 
 const btnEl = document.getElementById('btn');
+const timeEl = document.getElementById('time');
 
 console.log('im first');
 let count = 0;
@@ -12,8 +13,24 @@ setTimeout(() => {
 }, 3000);
 
 btnEl.addEventListener('click', () => {
-  console.log('paspaudei', count++);
+  console.log('paspaudei', ++count);
 });
 
 // setInterval((ka peleisti) , laikas milisek)
 // paleisti procesa kas kazkiek laiko
+// const interval1 = setInterval(logKazka, 2000);
+function logKazka() {
+  console.log('paspaudei', ++count);
+  if (count >= 10) {
+    clearInterval(interval1);
+  }
+}
+
+setInterval(tick, 1000);
+tick(); // tam kad nebutu 1s delay
+function tick() {
+  const nowObj = new Date();
+  const formattedTime = nowObj.toLocaleString('lt', { timeStyle: 'medium' });
+  //   console.log(formattedTime);
+  timeEl.textContent = formattedTime;
+}
